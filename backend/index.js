98be4a2a08c29c4058c4d3ec42da772508bf3204
Cristhian Zambrano <cz4785464@gmail.com>
2021-08-09 15:34:39 -0500

    creation of files user, role and board

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(process.env.PORT,()=>console.log("Backend server running on port: ", process.env.PORT));
+app.listen(process.env.PORT,()=>
+    console.log("Backend server running on port: ", process.env.PORT)
+    );
 
 dbConnection();
